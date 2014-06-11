604f0c479b437a11e9df13ea635b8fd058fc549e
SantiPugliese <santiagopugliese92@hotmail.com>
2014-06-11 01:18:47 -0300

    arreglado la mierda del filtro seleccion una facha

--- FrbaCommerce/Generar Publicacion/Generar_Publi.cs
+++ FrbaCommerce/Generar Publicacion/Generar_Publi.cs
@@ -8,31 +8,36 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace FrbaCommerce.Generar_Publicacion
-{
+{   
     public partial class Generar_Publi : Form
     {
+        
         private string usuario;
         public Generar_Publi(string usuarioPk)
         {
             InitializeComponent();
-            Utiles.Inicializar.comboBoxRubro(cmbRub);
+
             Utiles.Inicializar.comboBoxVisibilidad(cmbVisib);
             this.usuario = usuarioPk;
         }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
             Utiles.LimpiarTexto.LimpiarTextBox(this);
             Utiles.LimpiarTexto.BlanquearControls(this);
             Utiles.LimpiarTexto.SacarCheckBox(this);
+            textBox1.Enabled = false;
+            textBox1.BackColor = Color.WhiteSmoke;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Entidades.Ent_Publicacion publicacion = new Entidades.Ent_Publicacion();
             try
             {
-
-                Utiles.Validaciones.ValidarTipoDecimalPublicacion(textBox2, textBox3);
+                Utiles.Validaciones.ValidarTipoDecimalPublicacion(textBox1, textBox2, textBox3, textBox5);
+                inicializarPublicacion(publicacion);
         
             }
             catch (Exception ex)
@@ -59,13 +64,13 @@ namespace FrbaCommerce.Generar_Publicacion
             }
         }
 
-        private void inicializarCliente(Entidades.Ent_Publicacion publicacion)
+        private void inicializarPublicacion(Entidades.Ent_Publicacion publicacion)
         {
 
             publicacion.Visibilidad = Convert.ToInt16(cmbVisib.SelectedValue);
             publicacion.Tipo = Convert.ToString(cmbTipoPub.SelectedValue);
             publicacion.Stock = Convert.ToInt32(textBox2.Text);
-            publicacion.Rubro = Convert.ToInt16(cmbRub.SelectedValue);
+            publicacion.Rubro = Convert.ToInt16(textBox1.Text);
             publicacion.Precio = Convert.ToInt32(textBox3.Text);
             publicacion.Descripcion = Convert.ToString(textBox5.Text);
             publicacion.Permitir_Preguntas = Convert.ToBoolean(checkBox1.Checked);
@@ -73,6 +78,17 @@ namespace FrbaCommerce.Generar_Publicacion
             Datos.Dat_Publicacion.buscarDuracionVisibilidad(publicacion.Visibilidad, publicacion.Fecha_Venc);
 
         }
-  
+
+      
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Generar_Publicacion.BuscarRubro list = new Generar_Publicacion.BuscarRubro();
+            list.ShowDialog();
+            textBox1.Enabled = true;
+            textBox1.Text = list.Result;
+          
+        }
+
     }
 }
