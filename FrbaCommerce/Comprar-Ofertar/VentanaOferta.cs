8d474cb557b3798c1be24d8d358671e60ac02b33
nico14 <nicolas.pardo9314@gmail.com>
2014-06-12 16:56:05 -0300

    el mundial ha comenzado

--- FrbaCommerce/Comprar-Ofertar/VentanaOferta.cs
+++ FrbaCommerce/Comprar-Ofertar/VentanaOferta.cs
@@ -6,14 +6,47 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FrbaCommerce.Comprar_Ofertar
 {
     public partial class VentanaOferta : Form
     {
-        public VentanaOferta()
+        public VentanaOferta(Int32 codigoPub)
         {
             InitializeComponent();
+            codiigoPublicacion = codigoPub;
         }
+        private Int32 codiigoPublicacion;
+        private double precioBase;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Entidades.Ent_Oferta oferta = new FrbaCommerce.Entidades.Ent_Oferta();
+
+            try
+            {
+                validarValor(codiigoPublicacion,Convert.ToDouble(textBox1));
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       public void validarValor(Int32 codigoPub,double precioOfertado)
+       {
+           SqlConnection conn = DBConexion.obtenerConexion();
+           //Set the DataAdapter's query.
+           precioBase = Convert.ToDouble(new SqlDataAdapter("select precio from publicacion where id = codigoPub", conn));
+           if (precioOfertado > precioBase)
+           {
+               // agregarOferta
+           }
+           else
+           {
+              throw new  Excepciones.ValorMenor("El valor ingresado es menor a la última oferta realizada");
+           }
+       }
     }
 }
---
+++ FrbaCommerce/Entidades/Ent_Oferta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Entidades
+{
+    class Ent_Oferta
+    {
+        public Int16 Codigo_Pub { get; set; }
+        public DateTime Fecha { get; set; }
+        public Double Monto { get; set; }
+        public Decimal Id_Cli { get; set; }
+    }
+}
