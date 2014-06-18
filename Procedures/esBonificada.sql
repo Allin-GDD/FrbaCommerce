b62efb18728658fc240477d5b378fd356847f3e6
SantiPugliese <santiagopugliese92@hotmail.com>
2014-06-18 01:16:59 -0300

    agregado para que no tome migracion

--- Procedures/esBonificada.sql
+++ Procedures/esBonificada.sql
@@ -5,6 +5,6 @@ begin
 select Codigo,Publicador,Visibilidad_Cod,Fecha, RowNumber  
 from (select Codigo, Id, Publicador, Visibilidad_Cod, Fecha,row_number() over(order by Id,Publicador,Visibilidad_Cod,Fecha)as RowNumber
   from Publicacion) as Pub
-where RowNumber % 10 = 0
+where RowNumber % 10 = 0 AND Codigo > 68380
 order by Id, Publicador, Visibilidad_Cod, Fecha
 end
