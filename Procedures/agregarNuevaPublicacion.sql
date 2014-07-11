9ae5c8f64edf40d2d9cc0213ed3cf9e4e756b799
SantiPugliese <santiagopugliese92@hotmail.com>
2014-07-11 01:04:05 -0300

    agregarnueva proc mejorad

--- Procedures/agregarNuevaPublicacion.sql
+++ Procedures/agregarNuevaPublicacion.sql
@@ -4,6 +4,7 @@ CREATE PROCEDURE agregarNuevaPublicacion
 		@Tipo nvarchar(255),
 		@Stock numeric(18,0),
 		@Precio numeric(18,2),
+		@Rubro numeric(18,0),
 		@Descripcion nvarchar(255),
 		@Permitir_Preguntas bit,
 		@Fecha_Venc datetime,
@@ -11,7 +12,13 @@ CREATE PROCEDURE agregarNuevaPublicacion
  
 AS
 BEGIN
+	
+	DECLARE @idPub numeric(18,0)
+	
+	SET @idPub = (SELECT MAX(Codigo) from Publicacion) + 1
+	
 	INSERT INTO Publicacion(
+	Codigo,
 	Visibilidad_Cod,
 	Tipo,
 	Stock,
@@ -24,6 +31,7 @@ BEGIN
 	Usuario)
 	
 	VALUES(
+	@idPub,
 	@Visibilidad,
 	@Tipo,
 	@Stock,
@@ -34,5 +42,8 @@ BEGIN
 	'Borrador',
 	@Fecha_Venc,
 	@Usuario)
+	
+	INSERT INTO Publicacion_Rubro(id_Publicacion, id_Rubro)
+	VALUES(@idPub,@Rubro)
 
 END
