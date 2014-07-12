320222cf042d763f28f7b2e27a133ffdaed8d873
nico14 <nicolas.pardo9314@gmail.com>
2014-07-11 21:40:03 -0300

    refactoring

--- FrbaCommerce/Calificar Vendedor/Calificador.cs
+++ FrbaCommerce/Calificar Vendedor/Calificador.cs
@@ -26,12 +26,12 @@ namespace FrbaCommerce.Calificar_Vendedor
                 decimal puntaje = Convert.ToDecimal(textBox1.Text);
                 Utiles.Validaciones.ValidarTipoDecimal(textBox1);
                 validarPuntaje(puntaje);
-             //   decimal cod_calificacion = obternerCodCalificacionMax()+1;
+             
 
                 SqlConnection conn = DBConexion.obtenerConexion();
                 SqlCommand cmd = Utiles.SQL.crearProcedure("GD1C2014.dbo.agregarCalificacion", conn,
                 new SqlParameter("@Id", idCompra),
-              //  new SqlParameter("@Cal_Cod", cod_calificacion),
+            
                 new SqlParameter("@Cant_Est", puntaje),
                 new SqlParameter("@Desc", textBox2.Text));
                 int retorno = cmd.ExecuteNonQuery();
@@ -56,15 +56,6 @@ namespace FrbaCommerce.Calificar_Vendedor
             }
         }
 
-        private decimal obternerCodCalificacionMax()
-        {
-            decimal cod;
-            SqlConnection conexion = DBConexion.obtenerConexion();
-            SqlCommand Comando = new SqlCommand("select MAX(Calificacion_Codigo) from Compra", conexion);
-            SqlDataReader lectura = Comando.ExecuteReader();
-            lectura.Read();
-            cod = lectura.GetDecimal(0);
-            return cod;
-        }
+   
     }
 }
